b34f5b3e2b8b6c4da00e7fd5e10adc50badb8037
Enis Necipoğlu <doctorman_18@hotmail.com>
2017-12-28 16:19:16 +0300

    Add project files.

---
+++ TestAppSavableObject/TestAppSavableObject/MainPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.LocalData.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestAppSavableObject
+{
+    public class MainPageViewModel : SavableObject, INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            Load();
+            SaveCommand = new Command(()=>Save());
+        }
+
+        string name = "defaultName", surname = "defaultSurname";
+        double age;
+        bool isToggled;
+        public string Name
+        {
+            get => name;
+            set { name = value; OnPropertyChanged(); }
+        }
+        public string Surname
+        {
+            get => surname;
+            set { surname = value; OnPropertyChanged(); }
+        }
+        public double Age
+        {
+            get => age;
+            set { age = value; OnPropertyChanged(); }
+        }
+        public bool IsToggled
+        {
+            get => isToggled;
+            set { isToggled = value; OnPropertyChanged(); }
+        }
+        [IgnoreSave]
+        public Command SaveCommand { get; set; }
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
